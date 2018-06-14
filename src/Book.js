e59d9f813b77ebef2a60d996009c70c96da93809
Claire Walters <claire.walters@opower.com>
2018-06-13 22:09:47 -0400

    Dynamically generate options list and set selected shelf

--- src/Book.js
+++ src/Book.js
@@ -4,13 +4,13 @@ import BookshelfChanger from './BookshelfChanger';
 
 class Book extends Component {
     render() {
-        const {authors, imageLinks: {thumbnail}, title} = this.props.book;
+        const {authors, imageLinks: {thumbnail}, shelf, title} = this.props.book;
 
         return (
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }}></div>
-                    <BookshelfChanger />
+                    <BookshelfChanger shelf={shelf} />
                 </div>
                 <div className="book-title">{title}</div>
                 <div className="book-authors">{authors.join(', ')}</div>
--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -1,19 +1,43 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class BookshelfChanger extends Component {
     render() {
+        const {shelf} = this.props;
+
+        const statusValues = [
+            ['move', 'Move to...'],
+            ['currentlyReading', 'Currently Reading'],
+            ['wantToRead', 'Want to Read'],
+            ['read', 'Read'],
+            ['none', 'None']
+        ];
+
+        const isDisabled = (status) => status === 'move';
+
+        const isSelected = (status) => status === shelf;
+
         return (
             <div className="book-shelf-changer">
                 <select>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                    {statusValues.map(status => (
+                        <option
+                            key={status[0]}
+                            value={status[0]}
+                            disabled={isDisabled(status[0])}
+                            selected={isSelected(status[0])}
+                        >
+                            {status[1]}
+                        </option>
+                    ))};
                 </select>
             </div>
         );
     };
 };
+
+BookshelfChanger.propTypes = {
+    shelf: PropTypes.string.isRequired
+}
 
 export default BookshelfChanger;
