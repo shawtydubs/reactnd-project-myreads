ac03ae54da9a2466ef3450b081ed713dce96ca75
Claire Walters <claire.walters@opower.com>
2018-06-13 21:26:15 -0400

    Formatting in ListBooks

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -13,21 +13,21 @@ class ListBooks extends Component {
 
         return (
             <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <Bookshelf title="Currently Reading" books={currentlyReadingList} />
+                        <Bookshelf title="Want to Read" books={wantToReadList} />
+                        <Bookshelf title="Read" books={readList} />
+                    </div>
+                </div>
+                <div className="open-search">
+                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                </div>
             </div>
-            <div className="list-books-content">
-              <div>
-                <Bookshelf title="Currently Reading" books={currentlyReadingList} />
-                <Bookshelf title="Want to Read" books={wantToReadList} />
-                <Bookshelf title="Read" books={readList} />
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-      )
+        )
     };
 };
 
