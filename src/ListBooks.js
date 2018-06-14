ff1798818122685acc51117b0bd64cf66f687a11
Claire Walters <claire.walters@opower.com>
2018-06-13 21:07:48 -0400

    Build list of books for each shelf

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import Bookshelf from './Bookshelf';
 
 class ListBooks extends Component {
     render() {
+        const {books} = this.props;
+
+        const currentlyReadingList = _.filter(books, {shelf: 'currentlyReading'});
+        const readList = _.filter(books, {shelf: 'read'});
+        const wantToReadList = _.filter(books, {shelf: 'wantToRead'});
+
         return (
             <div className="list-books">
             <div className="list-books-title">
@@ -11,7 +18,9 @@ class ListBooks extends Component {
             </div>
             <div className="list-books-content">
               <div>
-                <Bookshelf />
+                <Bookshelf title="Currently Reading" books={currentlyReadingList} />
+                <Bookshelf title="Want to Read" books={wantToReadList} />
+                <Bookshelf title="Read" books={readList} />
               </div>
             </div>
             <div className="open-search">
