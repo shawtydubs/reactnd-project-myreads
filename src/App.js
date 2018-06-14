6831908cb5c992d437b84b3cec6044b4f8468e10
Claire Walters <claire.walters@opower.com>
2018-06-13 21:31:36 -0400

    Formatting in App.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,28 @@ import SearchBooks from './SearchBooks';
 import './App.css';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-    books: []
-  };
+    state = {
+        /**
+         * TODO: Instead of using this state variable to keep track of which page
+         * we're on, use the URL in the browser's address bar. This will ensure that
+         * users can use the browser's back and forward buttons to navigate between
+         * pages, as well as provide a good URL they can bookmark and share.
+         */
+        showSearchPage: false,
+        books: []
+    };
 
-  componentDidMount() {
-    BooksAPI.getAll().then(books => this.setState({books}));
-  };
+    componentDidMount() {
+        BooksAPI.getAll().then(books => this.setState({books}));
+    };
 
-  render() {
-    return (
-      <div className="app">
-          {this.state.showSearchPage ? (
-            <SearchBooks />
-          ) : (
-            <ListBooks books={this.state.books}/>
-        )}
-      </div>
-    )
-  }
-}
+    render() {
+        return (
+            <div className="app">
+                {this.state.showSearchPage ? <SearchBooks /> : <ListBooks books={this.state.books}/>}
+            </div>
+        )
+    };
+};
 
 export default BooksApp
